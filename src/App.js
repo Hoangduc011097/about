8e96dc45e7b025d64069c19a392043bb11d62ea5
hoangduc011097 <hhuuduc97@gmail.com>
2023-07-24 04:26:59 +0700

    update

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import AboutPage from './About/AboutPage';
+
+function App() {
+  return (
+    <div className="App">
+      <AboutPage/>
+    </div>
+  );
+}
+
+export default App;
